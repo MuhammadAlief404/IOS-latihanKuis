7b2e85bec3818a3b04600e88897465f148794eb7
prk <prk@BN71106.local>
2019-03-19 16:33:51 +0700

    first add

---
+++ latihanKuis/BuyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BuyViewController.swift
+//  latihanKuis
+//
+//  Created by prk on 19/03/19.
+//  Copyright © 2019 prk. All rights reserved.
+//
+
+import UIKit
+
+class BuyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
