8002599833cd94d3eb950082006fe4867bd274b1
prk <prk@bn71106.local>
2019-03-19 15:37:24 +0700

    Initial Commit

---
+++ latihanKuis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  latihanKuis
+//
+//  Created by prk on 19/03/19.
+//  Copyright © 2019 prk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
