765908267bef793f9d64a60dc260c54a9c36ce02
prk <prk@bn71106.local>
2019-03-19 16:58:38 +0700

    FINISH

--- latihanKuis/BuyViewController.swift
+++ latihanKuis/BuyViewController.swift
@@ -10,21 +10,53 @@ import UIKit
 
 class BuyViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var nama: UILabel!
+    @IBOutlet weak var harga: UILabel!
+    @IBOutlet weak var stock: UILabel!
+    @IBOutlet weak var txtQuantity: UITextField!
+    @IBOutlet weak var cash: UISegmentedControl!
+    
+    var data:Product?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        image.image = UIImage(named: data!.gambar)
+        nama.text = data!.nama
+        harga.text = "\(data!.harga)"
+        stock.text = "\(data!.stock)"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func buyClicked(_ sender: Any) {
+        if txtQuantity.text == "" {
+            showAlert(message: "Quantity harus di isi")
+        }
+        else if (Int(txtQuantity.text!) == nil)
+        {
+            showAlert(message: "Quantity harus berupa angka")
+        }
+        else if(Int(txtQuantity.text!)! > data!.stock)
+        {
+            showAlert(message: "Quantity melebihi jumlah stock")
+        }
+        else
+        {
+            var paymentType = cash.titleForSegment(at: cash.selectedSegmentIndex)
+            
+            var message = "Pembelian Berhasil \n"+"\(txtQuantity.text)\n" + "\(paymentType)"
+            
+            showAlert(message: message)
+        }
     }
-    */
-
+    @IBAction func kembali(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func showAlert(message:String)
+    {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
